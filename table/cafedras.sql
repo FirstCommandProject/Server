82c0e80af67d4512d92ef69dfc0f55a0ef24354e
sfmzerg <40385471+sfmzerg@users.noreply.github.com>
2021-06-11 19:38:09 +0300

    Add files via upload

---
+++ table/cafedras.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE expertsystem;
+USE expertsystem;
+CREATE TABLE Cafedras(
+    id INT UNIQUE,
+    title TEXT,
+    university TEXT(1024),
+    firstData MEDIUMTEXT,
+    secondData MEDIUMTEXT,
+    weights JSON
+);
