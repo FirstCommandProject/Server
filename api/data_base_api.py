ef35349af20d6cbdba374c6463dda984ef519070
sfmzerg <40385471+sfmzerg@users.noreply.github.com>
2021-06-14 16:07:16 +0300

    Исправил ошибки
    Вернул все функции до рабочего состояния

--- api/data_base_api.py
+++ api/data_base_api.py
@@ -20,7 +20,6 @@ def select_table_questions_id(id):
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу text
 def select_table_questions_text(text):
     cursor = database.cursor()
-    text = _remove_special_symbols(text)
     cursor.execute(f"SELECT * FROM Questions WHERE text = {text}")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -32,7 +31,6 @@ def select_table_questions_text(text):
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу tags
 def select_table_questions_tags(tags):
     cursor = database.cursor()
-    tags = _remove_special_symbols(tags)
     cursor.execute(f"SELECT * FROM Questions WHERE JSON_CONTAINS(tags, '[{tags}]')")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -66,7 +64,6 @@ def select_table_questions_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу login
 def select_table_users_login(login):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
     cursor.execute(f"SELECT * FROM Users WHERE login = '{login}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -78,7 +75,6 @@ def select_table_users_login(login):
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу password
 def select_table_users_password(password):
     cursor = database.cursor()
-    password = _remove_special_symbols(password)
     cursor.execute(f"SELECT * FROM Users WHERE password = '{password}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -90,7 +86,6 @@ def select_table_users_password(password):
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу name
 def select_table_users_name(name):
     cursor = database.cursor()
-    name = _remove_special_symbols(name)
     cursor.execute(f"SELECT * FROM Users WHERE name = '{name}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -101,7 +96,6 @@ def select_table_users_name(name):
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу surname
 def select_table_users_surname(surname):
-    surname = _remove_special_symbols(surname)
     cursor = database.cursor()
     cursor.execute(f"SELECT * FROM Users WHERE surname = '{surname}'")
     result = cursor.fetchall()
@@ -114,7 +108,6 @@ def select_table_users_surname(surname):
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу patronymic
 def select_table_users_patronymic(patronymic):
     cursor = database.cursor()
-    patronymic = _remove_special_symbols(patronymic)
     cursor.execute(f"SELECT * FROM Users WHERE patronymic = '{patronymic}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -126,7 +119,6 @@ def select_table_users_patronymic(patronymic):
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу university
 def select_table_users_university(university):
     cursor = database.cursor()
-    university = _remove_special_symbols(university)
     cursor.execute(f"SELECT * FROM Users WHERE university = '{university}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -149,7 +141,6 @@ def select_table_users_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Results по столбцу login
 def select_table_results_login(login):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
     cursor.execute(f"SELECT * FROM Users WHERE login = '{login}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -172,7 +163,6 @@ def select_table_results_weights():
 # Функция, которая выполняет sql запрос select к таблице Results по столбцу time
 def select_table_results_login(time):
     cursor = database.cursor()
-    time = _remove_special_symbols(time)
     cursor.execute(f"SELECT * FROM Results WHERE time = '{time}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -195,7 +185,6 @@ def select_table_results_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу id
 def select_table_cafedras_id(id):
     cursor = database.cursor()
-    id = _remove_special_symbols(id)
     cursor.execute(f"SELECT * FROM Cafedras WHERE id = '{id}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -207,7 +196,6 @@ def select_table_cafedras_id(id):
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу title
 def select_table_cafedras_title(title):
     cursor = database.cursor()
-    title = _remove_special_symbols(title)
     cursor.execute(f"SELECT * FROM Cafedras WHERE title = '{title}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -219,7 +207,6 @@ def select_table_cafedras_title(title):
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу university
 def select_table_cafedras_university(university):
     cursor = database.cursor()
-    university = _remove_special_symbols(university)
     cursor.execute(f"SELECT * FROM Cafedras WHERE university = '{university}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -231,7 +218,6 @@ def select_table_cafedras_university(university):
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу firstData
 def select_table_cafedras_firstData(firstData):
     cursor = database.cursor()
-    firstData = _remove_special_symbols(firstData)
     cursor.execute(f"SELECT * FROM Cafedras WHERE firstData = '{firstData}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -243,7 +229,6 @@ def select_table_cafedras_firstData(firstData):
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу secondData
 def select_table_cafedras_secondData(secondData):
     cursor = database.cursor()
-    secondData = _remove_special_symbols(secondData)
     cursor.execute(f"SELECT * FROM Cafedras WHERE secondData = '{secondData}'")
     result = cursor.fetchall()
     if len(result) == 0:
@@ -277,9 +262,6 @@ def select_table_cafedras_all_rows():
 # Функция, которая выполняет sql запрос insert к таблице Results
 def insert_table_results(login, weights, time):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    weights = _remove_special_symbols(weights)
-    time = _remove_special_symbols(time)
     cursor.execute(f"INSERT INTO Results VALUES('{login}', '{weights}', '{time}')")
     database.commit()
 
@@ -287,8 +269,6 @@ def insert_table_results(login, weights, time):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу login
 def upsert_table_users_login(last_login, new_login):
     cursor = database.cursor()
-    last_login = _remove_special_symbols(last_login)
-    new_login = _remove_special_symbols(new_login)
     cursor.execute(f"UPDATE Users SET login = '{new_login}' WHERE login = '{last_login}'")
     database.commit()
 
@@ -296,8 +276,6 @@ def upsert_table_users_login(last_login, new_login):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу password
 def upsert_table_users_password(login, new_password):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    new_password = _remove_special_symbols(new_password)
     cursor.execute(f"UPDATE Users SET password = '{new_password}' WHERE login = '{login}'")
     database.commit()
 
@@ -305,8 +283,6 @@ def upsert_table_users_password(login, new_password):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу name
 def upsert_table_users_name(login, new_name):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    new_name = _remove_special_symbols(new_name)
     cursor.execute(f"UPDATE Users SET name = '{new_name}' WHERE login = '{login}'")
     database.commit()
 
@@ -314,8 +290,6 @@ def upsert_table_users_name(login, new_name):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу surname
 def upsert_table_users_surname(login, new_surname):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    new_surname = _remove_special_symbols(new_surname)
     cursor.execute(f"UPDATE Users SET surname = '{new_surname}' WHERE login = '{login}'")
     database.commit()
 
@@ -323,8 +297,6 @@ def upsert_table_users_surname(login, new_surname):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу patronymic
 def upsert_table_users_patronymic(login, new_patronymic):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    new_patronymic = _remove_special_symbols(new_patronymic)
     cursor.execute(f"UPDATE Users SET patronymic = '{new_patronymic}' WHERE login = '{login}'")
     database.commit()
 
@@ -332,8 +304,6 @@ def upsert_table_users_patronymic(login, new_patronymic):
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу university
 def upsert_table_users_university(login, new_university):
     cursor = database.cursor()
-    login = _remove_special_symbols(login)
-    new_university = _remove_special_symbols(new_university)
     cursor.execute(f"UPDATE Users SET university = '{new_university}' WHERE login = '{login}'")
     database.commit()
 
@@ -345,5 +315,3 @@ if __name__ == '__main__':
         password="Zerg123456789ertyama_",
         database="expertsystem"
     )
-
-
