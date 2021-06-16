7e28ea741e447f5de796f3fdb86e6bbba71abbe6
sfmzerg <40385471+sfmzerg@users.noreply.github.com>
2021-06-16 11:40:37 +0300

    Обновил все функции
    Сделана защита sql injections

--- api/data_base_api.py
+++ api/data_base_api.py
@@ -1,55 +1,37 @@
 import mysql.connector
 
 
-# Функция, которая проверяет на наличие sql запроса DROP TABLE
-def _find_injection(string):
-    string = str(string)
-    if "DROP TABLE" in string or "--" in string:
-        return 1
-    else:
-        return 0
-
-
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу id
 def select_table_questions_id(id):
     cursor = database.cursor()
-    if _find_injection(id) == 0:
-        cursor.execute(f"SELECT * FROM Questions WHERE id = '{id}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Questions WHERE id = %s", (int(id), ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу text
 def select_table_questions_text(text):
     cursor = database.cursor()
-    if _find_injection(text) == 0:
-        cursor.execute(f"SELECT * FROM Questions WHERE text = '{text}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Questions WHERE text = %s", (text, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу tags
 def select_table_questions_tags(tags):
     cursor = database.cursor()
-    if _find_injection(tags) == 0:
-        cursor.execute(f"SELECT * FROM Questions WHERE JSON_CONTAINS(tags, '[{tags}]')")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Questions WHERE JSON_CONTAINS(tags, '[{tags}]')")
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Questions по столбцу weights
@@ -77,85 +59,67 @@ def select_table_questions_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу login
 def select_table_users_login(login):
     cursor = database.cursor()
-    if _find_injection(login) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE login = '{login}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE login = %s", (login, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу password
 def select_table_users_password(password):
     cursor = database.cursor()
-    if _find_injection(password) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE password = '{password}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE password = %s", (password, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу name
 def select_table_users_name(name):
     cursor = database.cursor()
-    if _find_injection(name) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE name = '{name}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE name = %s", (name, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу surname
 def select_table_users_surname(surname):
     cursor = database.cursor()
-    if _find_injection(surname) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE surname = '{surname}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE surname = %s", (surname, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу patronymic
 def select_table_users_patronymic(patronymic):
     cursor = database.cursor()
-    if _find_injection(patronymic) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE patronymic = '{patronymic}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE patronymic = %s", (patronymic, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по столбцу university
 def select_table_users_university(university):
     cursor = database.cursor()
-    if _find_injection(university) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE university = '{university}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE university = %s", (university, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Users по всем столбцам
@@ -172,13 +136,12 @@ def select_table_users_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Results по столбцу login
 def select_table_results_login(login):
     cursor = database.cursor()
-    if _find_injection(login) == 0:
-        cursor.execute(f"SELECT * FROM Users WHERE login = '{login}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Users WHERE login = %s", (login, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
+    else:
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Results по столбцу weights
@@ -195,15 +158,12 @@ def select_table_results_weights():
 # Функция, которая выполняет sql запрос select к таблице Results по столбцу time
 def select_table_results_login(time):
     cursor = database.cursor()
-    if _find_injection(time) == 0:
-        cursor.execute(f"SELECT * FROM Results WHERE time = '{time}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Results WHERE time = %s", (time, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Results по всем столбцам
@@ -220,71 +180,56 @@ def select_table_results_all_rows():
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу id
 def select_table_cafedras_id(id):
     cursor = database.cursor()
-    if _find_injection(id) == 0:
-        cursor.execute(f"SELECT * FROM Cafedras WHERE id = '{id}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Cafedras WHERE id = %s", (int(id), ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу title
 def select_table_cafedras_title(title):
     cursor = database.cursor()
-    if _find_injection(title) == 0:
-        cursor.execute(f"SELECT * FROM Cafedras WHERE title = '{title}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Cafedras WHERE title = %s", (title, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу university
 def select_table_cafedras_university(university):
     cursor = database.cursor()
-    if _find_injection(university) == 0:
-        cursor.execute(f"SELECT * FROM Cafedras WHERE university = '{university}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Cafedras WHERE university = %s", (university, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу firstData
 def select_table_cafedras_firstData(firstData):
     cursor = database.cursor()
-    if _find_injection(firstData) == 0:
-        cursor.execute(f"SELECT * FROM Cafedras WHERE firstData = '{firstData}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Cafedras WHERE firstData = %s", (firstData, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу secondData
 def select_table_cafedras_secondData(secondData):
     cursor = database.cursor()
-    if _find_injection(secondData) == 0:
-        cursor.execute(f"SELECT * FROM Cafedras WHERE secondData = '{secondData}'")
-        result = cursor.fetchall()
-        if len(result) == 0:
-            return None
-        else:
-            return result
+    cursor.execute(f"SELECT * FROM Cafedras WHERE secondData = %s", (secondData, ))
+    result = cursor.fetchall()
+    if len(result) == 0:
+        return None
     else:
-        print("Error")
+        return result
 
 
 # Функция, которая выполняет sql запрос select к таблице Cafedras по столбцу weights
@@ -312,71 +257,50 @@ def select_table_cafedras_all_rows():
 # Функция, которая выполняет sql запрос insert к таблице Results
 def insert_table_results(login, weights, time):
     cursor = database.cursor()
-    if _find_injection(login) == 0 and _find_injection(weights) == 0 and _find_injection(time) == 0:
-        cursor.execute(f"INSERT INTO Results VALUES('{login}', '{weights}', '{time}')")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"INSERT INTO Results VALUES(%s, %S, %s)", (login, weights, time))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу login
 def upsert_table_users_login(last_login, new_login):
     cursor = database.cursor()
-    if _find_injection(last_login) == 0 and _find_injection(new_login) == 0:
-        cursor.execute(f"UPDATE Users SET login = '{new_login}' WHERE login = '{last_login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET login = %s WHERE login = %s", (last_login, new_login))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу password
 def upsert_table_users_password(login, new_password):
     cursor = database.cursor()
-    if _find_injection(new_password) == 0 and _find_injection(login) == 0:
-        cursor.execute(f"UPDATE Users SET password = '{new_password}' WHERE login = '{login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET password = %s WHERE login = %s", (new_password, login))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу name
 def upsert_table_users_name(login, new_name):
     cursor = database.cursor()
-    if _find_injection(login) == 0 and _find_injection(new_name) == 0:
-        cursor.execute(f"UPDATE Users SET name = '{new_name}' WHERE login = '{login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET name = %s WHERE login = %s", (new_name, login))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу surname
 def upsert_table_users_surname(login, new_surname):
     cursor = database.cursor()
-    if _find_injection(login) == 0 and _find_injection(new_surname) == 0:
-        cursor.execute(f"UPDATE Users SET surname = '{new_surname}' WHERE login = '{login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET surname = %s WHERE login = %s", (new_surname, login))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу patronymic
 def upsert_table_users_patronymic(login, new_patronymic):
     cursor = database.cursor()
-    if _find_injection(login) == 0 and _find_injection(new_patronymic) == 0:
-        cursor.execute(f"UPDATE Users SET patronymic = '{new_patronymic}' WHERE login = '{login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET patronymic = %s WHERE login = %s", (new_patronymic, login))
+    database.commit()
 
 
 # Функция, которая выполняет sql запрос update к таблице Users по столбцу university
 def upsert_table_users_university(login, new_university):
     cursor = database.cursor()
-    if _find_injection(login) == 0 and _find_injection(new_university) == 0:
-        cursor.execute(f"UPDATE Users SET university = '{new_university}' WHERE login = '{login}'")
-        database.commit()
-    else:
-        print("Error")
+    cursor.execute(f"UPDATE Users SET university = %s WHERE login =  %s", (new_university, login))
+    database.commit()
 
 
 if __name__ == '__main__':
