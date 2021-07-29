adfb04fea20d39875e4abc6d56fa40c4d97fd121
Alex <alex.zv.ev@gmail.com>
2021-07-29 09:44:12 +0300

    Необходимые функции

--- scripts/core.py
+++ scripts/core.py
@@ -1,5 +1,5 @@
 import random
-import database_api
+import Server.database_api.database_api as database_api
 
 
 # Нормализует веса в состоянии по принципу нормализации вектора
@@ -18,13 +18,27 @@ def _update_answered_list(user_session_data, question_id):
     user_session_data['answered'].append(question_id)
 
 
+def _choose_random_question_by_tag(user_session_data, relevant_tag):
+    raw_array_representation = ''  # Example: 2, 6, 10, 11
+    for answered_id in user_session_data['answered']:
+        if raw_array_representation != '':
+            raw_array_representation.append(', ')
+        raw_array_representation.append(str(answered_id))
+
+    query = f"SELECT * FROM ExpertSystem.Questions WHERE " \
+            f"JSON_CONTAINS(tags, '[{relevant_tag}]') AND id NOT IN ({raw_array_representation}) ORDER BY RAND() LIMIT 1"
+
+    random_question = database_api.make_custom_request(query)
+    return random_question
+
+
 # Выбирает актуальный тег для пользователя, основываясь на уже известных предпочтениях
 # Поиск производится по тегам, указанным в вопросах
 # Выбирается один из тегов, который, вероятно, интересен человеку (хотя иногда может быть неинтересен)
 # Пармаетр strictness означает насколько строго будет выбираться (0 - рандомно, 1 - максимально строго)
 # Крайне не рекомендуется выбирать значения strictness в пределах от 0.000...1 до 0.2
 # Возвращает тег
-def choose_relevant_tag(user_session_data, strictness=0.75) -> str:
+def _choose_relevant_tag(user_session_data, strictness) -> str:
     try:
         assert user_session_data['weights']
         assert 0 <= strictness <= 1
@@ -40,6 +54,26 @@ def choose_relevant_tag(user_session_data, strictness=0.75) -> str:
                     user_session_data['weights'][tags[chosen_tag_index]] * strictness)
         chosen_tag_index = random.randint(0, len(tags) - 1)
     return tags[chosen_tag_index]
+
+
+# Выбирает актуальный вопрос для пользователя, основываясь на уже известных предпочтениях
+# Возвращает id
+def _choose_relevant_question_id(user_session_data, strictness) -> int:
+    relevant_tag = _choose_relevant_tag(user_session_data, strictness=strictness)
+    relevant_question_id = _choose_random_question_by_tag(user_session_data, relevant_tag)['id']
+    return relevant_question_id
+
+
+# Выбирает актуальный вопрос для пользователя, основываясь на уже известных предпочтениях
+# Поиск производится по тегам, указанным в вопросах
+# Выбирается один из вопрос, который, вероятно, интересен человеку (хотя иногда может быть неинтересен)
+# Пармаетр strictness означает насколько строго будет выбираться (0 - рандомно, 1 - максимально строго)
+# Крайне не рекомендуется выбирать значения strictness в пределах от 0.000...1 до 0.2
+# Возвращает вопрос в виде json
+def choose_relevant_question(user_session_data, strictness=0.75) -> dict:
+    relevant_tag = _choose_relevant_tag(user_session_data, strictness=strictness)
+    relevant_question = _choose_random_question_by_tag(user_session_data, relevant_tag)
+    return relevant_question
 
 
 # Ratio 1 - полностью применить. Пример: Математика: 1, в вопросе математика: 2, в итоге в состоянии будет математика: 2
@@ -58,7 +92,7 @@ def update_weights(user_session_data, question_id, ratio) -> dict:
     if ratio == 0:
         return user_session_data
 
-    question: dict = database_api.get_question(question_id)  # Вызов API для получения нужного вопроса
+    question: dict = database_api.select_question_by_id(question_id)  # Вызов API для получения нужного вопроса
     question_keys = question['weights'].keys()
 
     try:
