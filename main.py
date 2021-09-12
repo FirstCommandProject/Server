b2aa94b081ce5f5609e7024337e5f97cd4f35e14
Dima <40385471+sfmzerg@users.noreply.github.com>
2021-09-12 12:13:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import *
+from database_api import *
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/undefined/login', methods=['POST'])
+def login():
+    value = request.get_json(silent=True)
+    if authorize_user(value['email'], value['password']) == [(1,)]:
+        result = select_user_data(value['email'])
+        dictionary_for_work = {}
+        dictionary_for_work.update({'200': result})
+        return dictionary_for_work
+    else:
+        print('Неверный логин или пароль')
+        return '400'
+
+
+@app.route('/sign/up/undefined/registration', methods=['POST'])
+def registration_page2():
+    value = request.get_json(silent=True)
+    if authorize_user(value['email'], value['password']) != [(1,)]:
+        add_new_user(value['email', value['password'], value['firsName'], value['secondName'], value['thirdName'], value['university']])
+        return '200'
+    else:
+        return '400'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
