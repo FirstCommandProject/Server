037cd346c5fea838507bf5cca4f8721892ab506a
Dima <40385471+sfmzerg@users.noreply.github.com>
2021-09-14 18:35:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+
 class LoginModel(BaseModel):
     email: str
     password: str
+
 
 class RegistrModel(BaseModel):
     email: str
@@ -11,3 +13,12 @@ class RegistrModel(BaseModel):
     secondName: str
     thirdName: str
     university: str
+
+
+class CafedraModel(BaseModel):
+    id: int
+    title: str
+    university: str
+    firstData: str
+    secondData: str
+    weights: dict
