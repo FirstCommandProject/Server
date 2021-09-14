1165a18bfa7d838d21b9e4be798bbd569966e9dd
Dima <40385471+sfmzerg@users.noreply.github.com>
2021-09-14 18:41:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from database_api.database_api import *
+from database_api import *
 import uvicorn
 from models import *
 
@@ -15,14 +15,15 @@ app.add_middleware(
     allow_headers=origins,
 )
 
+
 @app.post('/login', status_code=200)
 async def login(body: LoginModel):
     if authorize_user(body.email, body.password) == [(1,)]:
         result=select_user_data(body.email)
         result_dictionary={}
         result_dictionary.update(
-            statusCode='200', 
-            data=result[0]
+            statusCode='200',
+            data=result
         )
         return result_dictionary
     else:
@@ -34,19 +35,32 @@ async def login(body: LoginModel):
 async def registration(body: RegistrModel):
 
     if authorize_user(body.email, body.password) != [(1,)]:
-        result=add_new_user(body.email, body.password, body.firstName, body.secondName, body.thirdName, body.university)
-        result_dictionary={}
-        # TO-DO: return user in after_new_user function
-        result=select_user_data(body.email)
+        add_new_user(body.email, body.password, body.firstName, body.secondName, body.thirdName, body.university)
+        result = select_user_data(body.email)
+        result_dictionary = {}
         result_dictionary.update(
-            statusCode='200', 
-            data=result[0]
+            statusCode='200',
+            data=result
         )
         return result_dictionary
     else:
         print('Ошибка регистрации')
         raise HTTPException(status_code=400, detail="Такой пользователь уже существует")
 
+
+@app.post('/departments', status_code=200)
+async def departments(body:CafedraModel):
+    if select_cafedra_by_id(body.id):
+        result = select_user_data(body.email)
+        result_dictionary = {}
+        result_dictionary.update(
+            statusCode='200',
+            data=result
+        )
+        return result_dictionary
+    else:
+        print('Ошибка')
+        raise HTTPException(status_code=400, detail="Такой кафедры не существует")
 
 if __name__ == "__main__":
     uvicorn.run('main:app', port=5000, reload=True)
