97b6a32ed5d303a164c7e170dd8f60edba39f007
Alex <alex.zv.ev@gmail.com>
2021-09-19 12:17:36 +0300

    Make response as dict

--- scripts/core.py
+++ scripts/core.py
@@ -77,7 +77,10 @@ def choose_relevant_question(user_session_data, strictness=0.75) -> dict:
         relevant_tag = _choose_relevant_tag(user_session_data, strictness=strictness)
         relevant_question = _choose_random_question_by_tag(user_session_data, relevant_tag)
         assert relevant_question is not []
-        return relevant_question
+        res_dict = {'id': relevant_question[0][0], 'title': relevant_question[0][1],
+                    'tags': relevant_question[0][2],
+                    'weights': relevant_question[0][3]}
+        return res_dict
     except:
         return None
 
